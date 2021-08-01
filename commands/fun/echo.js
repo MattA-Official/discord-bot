f47419caacfe292e06645a286e14d317c44583c9
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-08-01 22:31:23 +0100

    🐛 Fixed Bug Causing RangeError

--- commands/fun/echo.js
+++ commands/fun/echo.js
@@ -21,12 +21,16 @@ export const permissions = [
 ];
 
 export const slash = true;
+export const message = true;
 
 export const interaction = async (client, interaction) => {
 	const reply = interaction.options.getString('input');
 	await interaction.reply(reply);
 };
 export const execute = async (client, msg, args) => {
+	if (args.length === 0)
+		return await msg.reply('You need to pass an input to echo back!');
+
 	const reply = args.join(' ');
 	await msg.reply(reply);
 };
