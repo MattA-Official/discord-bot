593a93c405cbda85634c4fca9be9abf6adc29b66
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-09-10 17:22:32 +0100

    🛢 SQLite DB

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { Client, Intents, Collection } from 'discord.js';
-import Keyv from 'keyv';
 import 'dotenv/config';
 
 import { commands, buttons, events, menus } from './utils/registry.js';
@@ -11,13 +10,6 @@ const client = new Client({
 client.commands = new Collection();
 client.buttons = new Collection();
 client.menus = new Collection();
-if (process.env.REDIS_URI) client.db = new Keyv(process.env.REDIS_URI);
-else {
-	console.log(`REDIS_URI not set, using in-memory store`);
-	client.db = new Keyv();
-}
-
-client.db?.on('error', (err) => console.error('DB connection error:', err));
 
 await commands(client, './commands');
 await buttons(client, './components/buttons');
